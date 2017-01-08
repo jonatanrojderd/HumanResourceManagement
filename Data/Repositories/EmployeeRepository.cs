89b8e3648b6e112d2a19f7cb0994563e8f40782d
Valentine\ADHD <jonatanrojderd@gmail.com>
2017-01-08 18:05:56 +0100

    Added EmployeeRepository.

---
+++ Data/Repositories/EmployeeRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Data.Context;
+using Data.Models;
+
+namespace Data.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public void Create(Employee employee)
+        {
+            using (var context = new HrmContext())
+            {
+                context.Employees.Add(new Employee
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = employee.FirstName,
+                    LastName = employee.LastName,
+                    Email = employee.Email,
+                    Salary = employee.Salary,
+                    DateOfBirth = employee.DateOfBirth,
+                    Absence = employee.Absence
+                });
+                context.SaveChanges();
+            }
+        }
+
+        public async Task<IList<Employee>> ReadAsync()
+        {
+            using (var context = new HrmContext())
+            {
+                return await context.Employees.ToListAsync();
+            }
+        }
+    }
+}
---
+++ Data/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Data.Models;
+
+namespace Data.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        void Create(Employee employee);
+        Task<IList<Employee>> ReadAsync();
+    }
+}
