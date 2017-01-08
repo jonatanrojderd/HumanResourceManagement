cc3bd8c91d4e9fd323359c08e55347dd5436c8e2
Valentine\ADHD <jonatanrojderd@gmail.com>
2017-01-08 17:15:35 +0100

    Added Employee and Department models.

---
+++ Data/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.Models
+{
+    public class Department
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual IList<Employee> Employees { get; set; }
+    }
+}
---
+++ Data/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Data.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool Absence { get; set; }
+
+        public virtual Department Department { get; set; }
+    }
+}
