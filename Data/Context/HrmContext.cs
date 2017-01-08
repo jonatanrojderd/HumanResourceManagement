6d6df7e20dbeefbbf137d0036b5fde43ed42b9b4
Valentine\ADHD <jonatanrojderd@gmail.com>
2017-01-08 17:33:17 +0100

    Added databasecontext and enabled migrations.

---
+++ Data/Context/HrmContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+using Data.Models;
+
+namespace Data.Context
+{
+    public class HrmContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Data/Migrations/201701081632066__InitialCreate.cs
@@ -0,0 +1,46 @@
+namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Departments",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Email = c.String(),
+                        Salary = c.Int(nullable: false),
+                        DateOfBirth = c.DateTime(nullable: false),
+                        Absence = c.Boolean(nullable: false),
+                        Department_Id = c.Guid(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Departments", t => t.Department_Id)
+                .Index(t => t.Department_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Employees", "Department_Id", "dbo.Departments");
+            DropIndex("dbo.Employees", new[] { "Department_Id" });
+            DropTable("dbo.Employees");
+            DropTable("dbo.Departments");
+        }
+    }
+}
